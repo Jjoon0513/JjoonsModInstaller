10ebda02450749562679762effd1ee60c2685069
쭌이Jjoon <ijjun244@gmail.com>
2023-12-26 00:11:57 +0900

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,138 @@
+﻿// MainWindow.xaml.cs
+using System;
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace modinsteller
+{
+    public partial class MainWindow : Window
+    {
+        private string selectedPath;
+
+        public MainWindow()
+        {
+            try
+            {
+                string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                string newFilePath = Path.Combine(appDataPath, "Jjoon Minecraft Insteller", "Old_Version.json");
+
+                Directory.CreateDirectory(Path.GetDirectoryName(newFilePath));
+                if (!File.Exists(newFilePath))
+                {
+                    File.WriteAllText(newFilePath, "{ \"modpack_ver\": \"2023-12-25\"}");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+            InitializeComponent();
+        }
+
+        private void OnBrowseButtonClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.ShowDialog();
+            if (dialog.SelectedPath != "")
+            {
+                string selected = dialog.SelectedPath;
+                lootrabel.Content = selected;
+                selectedPath = selected;
+            }
+        }
+
+        private async void ver_check_click(object sender, RoutedEventArgs e)
+        {
+            if (selectedPath == null)
+            {
+                System.Windows.Forms.MessageBox.Show("먼저 마인크래프트 mods 파일을 지정해 주십시오", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                    string downloadPath = Path.Combine(appDataPath, "Jjoon Minecraft Insteller", "Current_Version.json");
+
+                    await DownloadFileInBackground("https://github.com/Jjoon0513/Minecraftmoddata/raw/main/ver.json", downloadPath);
+
+                    using (StreamReader file = File.OpenText(Path.Combine(appDataPath, "Jjoon Minecraft Insteller", "Current_Version.json")))
+                    using (StreamReader file1 = File.OpenText(Path.Combine(appDataPath, "Jjoon Minecraft Insteller", "Old_Version.json")))
+                    {
+                        using (JsonTextReader reader = new JsonTextReader(file))
+                        using (JsonTextReader reader1 = new JsonTextReader(file1))
+                        {
+                            JObject Current_Version = await JObject.LoadAsync(reader);
+                            JObject Old_Version = await JObject.LoadAsync(reader1);
+
+                            if (Current_Version["modpack_ver"].ToString() == Old_Version["modpack_ver"].ToString())
+                            {
+                                System.Windows.Forms.MessageBox.Show("현재 최신버전 입니다.", "버전확인", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            }
+                            else
+                            {
+                                System.Windows.Forms.MessageBox.Show($"최신버전: {Current_Version["modpack_ver"].ToString()} \n현재버전: {Old_Version["modpack_ver"].ToString()}", "버전확인", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                                MessageBoxResult result = System.Windows.MessageBox.Show("최신버전으로 다운로드 하시겠습니까?", "버전확인", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                                if (result == MessageBoxResult.Yes)
+                                {
+                                    string minecraftVer = Current_Version["minecraft_ver"].ToString();
+                                    string modpackVer = Current_Version["modpack_ver"].ToString();
+
+                                    string downloadUrl = $"https://archive.org/download/Jjoon-{minecraftVer}-{modpackVer}/mods-{minecraftVer}.zip";
+
+                                    // 다운로드 경로 지정
+                                    string downloadFilePath = Path.Combine(appDataPath, "Jjoon Minecraft Insteller", "mods.zip");
+
+                                    // 백그라운드에서 다운로드 수행
+                                    var window1 = new Window1(downloadUrl, downloadFilePath, selectedPath);
+                                    window1.ShowDialog();
+                                }
+                            }
+                        }
+                    }
+                    string oldfile = $"{appDataPath}\\Jjoon Minecraft Insteller\\Old_Version.json";
+                    string newfile = $"{appDataPath}\\Jjoon Minecraft Insteller\\Current_Version.json";
+                    File.Delete(oldfile);
+                    File.Move(newfile, oldfile, true);
+                }
+                catch (WebException ex)
+                {
+                    // WebException 처리
+                    if (ex.Response is HttpWebResponse response)
+                    {
+                        System.Windows.Forms.MessageBox.Show($"다운로드 중 오류가 발생했습니다. 응답 코드: {response.StatusCode}", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        System.Windows.Forms.MessageBox.Show($"다운로드 중 오류가 발생했습니다: {ex.Message}", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    // 내부 예외 확인
+                    if (ex.InnerException != null)
+                    {
+                        System.Windows.Forms.MessageBox.Show($"내부 오류: {ex.InnerException.Message}", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    System.Windows.Forms.MessageBox.Show($"다운로드 중 오류가 발생했습니다: {ex.Message}", "에러", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private async Task DownloadFileInBackground(string url, string downloadPath)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                await webClient.DownloadFileTaskAsync(new Uri(url), downloadPath);
+            }
+        }
+    }
+}
---
+++ Window1.xaml.cs
@@ -0,0 +1,111 @@
+﻿// Window1.xaml.cs
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace modinsteller
+{
+    public partial class Window1 : Window
+    {
+        private readonly string _downloadUrl;
+        private readonly string _downloadPath;
+        private readonly string _selectedPath;
+
+        public Window1(string downloadUrl, string downloadPath, string selectedPath)
+        {
+            InitializeComponent();
+            _downloadUrl = downloadUrl;
+            _downloadPath = downloadPath;
+            _selectedPath = selectedPath;
+
+            // 백그라운드에서 다운로드 및 압축 해제 수행
+            DownloadAndExtractInBackground();
+        }
+
+        private async void DownloadAndExtractInBackground()
+        {
+            try
+            {
+                string tempFolderPath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
+                // WebClient 생성
+                using (WebClient webClient = new WebClient())
+                {
+                    // DownloadProgressChanged 이벤트 등록
+                    webClient.DownloadProgressChanged += WebClient_DownloadProgressChanged;
+
+                    // 파일 다운로드
+                    await webClient.DownloadFileTaskAsync(new Uri(_downloadUrl), _downloadPath);
+                }
+
+                // 압축 해제
+                await ExtractZipAsync(_downloadPath, _selectedPath);
+
+                // 다운로드 및 압축 해제 완료 메시지
+                MessageBox.Show("다운로드 및 압축 해제가 완료되었습니다.");
+                string appDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"오류가 발생했습니다: {ex.Message}");
+            }
+            finally
+            {
+                // 창 닫기
+                Close();
+            }
+        }
+
+        private async Task ExtractZipAsync(string zipFilePath, string extractPath)
+        {
+            try
+            {
+                // Ensure the target extraction directory exists
+                Directory.CreateDirectory(extractPath);
+
+                // Extract the zip file
+                await Task.Run(() => ZipFile.ExtractToDirectory(zipFilePath, extractPath));
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"압축 해제 중 오류가 발생했습니다: {ex.Message}");
+            }
+        }
+
+        private void MoveFiles(string sourceDirectory, string destinationDirectory)
+        {
+            if (!Directory.Exists(destinationDirectory))
+            {
+                Directory.CreateDirectory(destinationDirectory);
+            }
+
+            foreach (string file in Directory.GetFiles(sourceDirectory))
+            {
+                string fileName = Path.GetFileName(file);
+                string destinationPath = Path.Combine(destinationDirectory, fileName);
+
+                // 파일을 이동하기 전에 기존에 동일한 이름의 파일이 있다면 삭제
+                if (File.Exists(destinationPath))
+                {
+                    File.Delete(destinationPath);
+                }
+
+                File.Move(file, destinationPath);
+            }
+        }
+
+        // 다운로드 진행 상태를 업데이트하는 이벤트 핸들러
+        private void WebClient_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            // 프로세스바의 값을 다운로드 진행 상황에 맞게 업데이트
+            ProgressBar.Value = e.ProgressPercentage;
+
+            // 상태 레이블 업데이트
+            StatusLabel.Content = $"다운로드 중... {e.ProgressPercentage}% 완료";
+        }
+    }
+}
